49476f173d0ee80dc06ee54173357b416288aee1
Miguel Castela <miguelcastela23@gmail.com>
2026-04-22 16:19:34 -0300

    Update models.py

--- BetterTFinal 1.0/core/models.py
+++ BetterTFinal 1.0/core/models.py
@@ -1,32 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Post(models.Model):#exemplo de herança #
+# 1. ESTA É A CLASSE ABSTRATA
+# Ela centraliza o campo 'data' que existia em todos os seus modelos
+class BaseModelo(models.Model):
+    data = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True  # <--- Isso define que ela é uma Classe Abstrata
+
+# 2. AGORA OS MODELOS HERDAM DA BaseModelo
+class Post(BaseModelo): # Herança da classe abstrata
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
     conteudo = models.TextField(max_length=280)
-    data = models.DateTimeField(auto_now_add=True)
     
-    abstract = true
     curtidas = models.ManyToManyField(User, related_name='curtidas', blank=True)
     dislikes = models.ManyToManyField(User, related_name='dislikes', blank=True)
     salvados = models.ManyToManyField(User, related_name='salvados', blank=True)
-
-
     republicado_de = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
 
-class Comentario(models.Model):
+class Comentario(BaseModelo): # Herança da classe abstrata
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comentarios')
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
     texto = models.TextField()
-    data = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.autor.username} em {self.post.id}'
 
-class Suporte(models.Model):
-
+class Suporte(BaseModelo): # Herança da classe abstrata
     usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     mensagem = models.TextField()
-    data = models.DateTimeField(auto_now_add=True)
-
-
+    
