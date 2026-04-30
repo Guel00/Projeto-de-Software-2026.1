597badc9cc148c0e57584747b9fb66866de254a5
Miguel Castela <miguelcastela23@gmail.com>
2026-04-29 23:29:10 -0300

    Refactor models to remove abstract base class
    Removed abstract base class and added data field directly to Post, Comentario, and Suporte models.

--- BetterTFinal 1.0/core/models.py
+++ BetterTFinal 1.0/core/models.py
@@ -1,32 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# 1. ESTA É A CLASSE ABSTRATA
-class BaseModelo(models.Model):
-    data = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        abstract = True  
-
-
-class Post(BaseModelo): # Herança da classe abstrata
+class Post(models.Model):
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
     conteudo = models.TextField(max_length=280)
-    
+    data = models.DateTimeField(auto_now_add=True)
+
     curtidas = models.ManyToManyField(User, related_name='curtidas', blank=True)
     dislikes = models.ManyToManyField(User, related_name='dislikes', blank=True)
     salvados = models.ManyToManyField(User, related_name='salvados', blank=True)
+
+
     republicado_de = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
 
-class Comentario(BaseModelo): # Herança da classe abstrata
+class Comentario(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comentarios')
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
     texto = models.TextField()
+    data = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.autor.username} em {self.post.id}'
 
-class Suporte(BaseModelo): # Herança da classe abstrata
+class Suporte(models.Model):
+
     usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     mensagem = models.TextField()
-    
+    data = models.DateTimeField(auto_now_add=True)
